81132b8382b7e15edddef46ee9695085f15172d5
Stheven Cabral <stheven.cabral@gmail.com>
2020-05-18 16:22:56 -0700

    05182020 1622

--- src/Components/PhotoContainer.js
+++ src/Components/PhotoContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoContainer = () => {
   return (
     <div class="photo-container">
         <h2>Results</h2>
         <ul>
+          <Photo />
         </ul>
       </div>
   )
