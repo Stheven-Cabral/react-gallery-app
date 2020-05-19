8b8844e720cd467ef22843deffc40eea6df2c164
Stheven Cabral <stheven.cabral@gmail.com>
2020-05-18 21:24:04 -0700

    05182020  2123

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter,
-  Router,
+  Route,
   Switch
 } from 'react-router-dom';
 
 import './css/index.css';
-import apiKey from './data/config';
+// import apiKey from './data/config';
 import SearchForm from './components/SearchForm';
 import Navigation from './components/Navigation';
 import PhotoContainer from './components/PhotoContainer';
@@ -15,11 +15,21 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="container">
-        <SearchForm />
-        <Navigation />
-        <PhotoContainer />
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <SearchForm />
+          <Navigation />
+          <PhotoContainer />
+
+          <Switch>
+            {/* Use search :topic for PhotoContainer component in order to search for that specific topic. */}
+            <Route path="/:topic" component={PhotoContainer}/>
+            <Route path="/cats" render={ () => <PhotoContainer topic="cats" /> } />
+            <Route path="/dogs" render={ () => <PhotoContainer topic="dogs" /> } />
+            <Route path="/computers" render={ () => <PhotoContainer topic="computers" /> } />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+
+
 export default class SearchForm extends Component {
   render() {
     return (
